7d57a72796fd50733d78afb5bfe559ca2d70fde3
GeriPopova28 <165501767+GeriPopova28@users.noreply.github.com>
2025-06-24 20:36:55 +0300

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,13 @@
+import styles from '../styles/hero.module.css'; 
+export default function Hero() {
+  return (
+    <section className={styles.hero}> {}
+      <div className={styles.container}> {}
+        <h1><i>Welcome — We are CBES LTD.</i></h1>
+        <h2><i>Nice to meet you</i></h2>
+        <p>Some history about us!</p>
+      </div>
+    </section>
+  );
+}
+
